68cd1aed2eeee287de08287c373631f7617303af
Crisano97 <97keb6@gmail.com>
2022-06-13 15:56:02 +0200

    nome, cognome e colore preferito sull'htm

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@ const userName = prompt('Inserisci il tuo nome');
 
 console.log(userName)
 
+document.getElementById('user-name').innerHTML = userName;
+
 
 //Chiediamo all'utente il suo cognome;
 const userSurname = prompt('Inserisci il tuo cognome');
 
 console.log(userSurname)
 
+document.getElementById('user-surname').innerHTML = userSurname;            
+
 //Chiediamo all'utente il suo colore preferito;
 const userFavouriteColor = prompt('Inserisci il tuo colore preferito');
 
 console.log(userFavouriteColor)
+
+document.getElementById('user-favourite-color').innerHTML = userFavouriteColor;
