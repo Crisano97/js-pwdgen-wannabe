4a2793ab65f10a28beb109fdf6059140d2995a6f
Crisano97 <97keb6@gmail.com>
2022-06-13 16:59:38 +0200

    password generator

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
 // Chiediamo all'utente il suo nome;
-const userName = prompt('Inserisci il tuo nome');
+const userName = prompt('Nome');
 
-console.log(userName)
+console.log(userName);
 
 document.getElementById('user-name').innerHTML = userName;
 
 
 //Chiediamo all'utente il suo cognome;
-const userSurname = prompt('Inserisci il tuo cognome');
+const userSurname = prompt('Cognome');
 
-console.log(userSurname)
+console.log(userSurname);
 
 document.getElementById('user-surname').innerHTML = userSurname;            
 
 //Chiediamo all'utente il suo colore preferito;
-const userFavouriteColor = prompt('Inserisci il tuo colore preferito');
+const userFavouriteColor = prompt('Colore preferito');
 
-console.log(userFavouriteColor)
+console.log(userFavouriteColor);
 
 document.getElementById('user-favourite-color').innerHTML = userFavouriteColor;
+
+// Chiediamo all'utente quanti anni ha;
+const userAge = prompt('Anni');
+
+console.log(userAge);
+
+document.getElementById('user-age').innerHTML = userAge;
+
+//Generiamo una password dai valori espressi dall'utente;
+
+let userPassword = userName + userSurname + userFavouriteColor + userAge;
+
+document.getElementById('user-password').innerHTML = userPassword
