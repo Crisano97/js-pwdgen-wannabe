d50958bb00d340848eccd61814f272659fc7d9d8
Crisano97 <97keb6@gmail.com>
2022-06-13 15:24:42 +0200

    prova solo js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Chiediamo all'utente il suo nome;
+const userName = prompt('Inserisci il tuo nome');
+
+console.log(userName)
+
+
+//Chiediamo all'utente il suo cognome;
+const userSurname = prompt('Inserisci il tuo cognome');
+
+console.log(userSurname)
+
+//Chiediamo all'utente il suo colore preferito;
+const userFavouriteColor = prompt('Inserisci il tuo colore preferito');
+
+console.log(userFavouriteColor)
